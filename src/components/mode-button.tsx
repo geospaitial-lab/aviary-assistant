9dc1745160627017878aa1e6c18127b2a23cf2b0
mrsmrynk <marius.maryniak@w-hs.de>
2025-06-10 12:58:09 +0200

    refactor

--- src/components/mode-button.tsx
+++ src/components/mode-button.tsx
@@ -21,8 +21,8 @@ export function ModeButton() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <Sun className="scale-100 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Design ändern</span>
         </Button>
       </DropdownMenuTrigger>
