9ed1f392047f961309b6673058f0a1456d7f8358
mrsmrynk <marius.maryniak@w-hs.de>
2025-06-26 16:00:02 +0200

    add `FileForm`

---
+++ src/components/assistant/file/form.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+
+import {
+  type FileFormSchema,
+  fileFormSchema,
+} from "@/components/assistant/file/schema"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+export function FileForm() {
+  const form = useForm<FileFormSchema>({
+    resolver: zodResolver(fileFormSchema),
+    defaultValues: {
+      file: undefined,
+    },
+    mode: "onSubmit",
+    reValidateMode: "onSubmit",
+  })
+
+  function onSubmit(values: FileFormSchema) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        noValidate
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="grid gap-4"
+      >
+        <FormField
+          control={form.control}
+          name="file"
+          render={({ field: { onChange, value, ...field } }) => (
+            <FormItem>
+              <FormLabel>Datei</FormLabel>
+              <FormControl>
+                <Input
+                  type="file"
+                  onChange={(e) => {
+                    const file = e.target.files?.[0]
+                    onChange(file)
+                  }}
+                  {...field}
+                />
+              </FormControl>
+              <div className="min-h-[1.25rem]">
+                <FormMessage />
+              </div>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Anzeigen</Button>
+      </form>
+    </Form>
+  )
+}
