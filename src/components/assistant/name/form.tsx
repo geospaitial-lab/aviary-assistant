f277d63f26bab537b4d05d3e75a830be195b7cc4
mrsmrynk <marius.maryniak@w-hs.de>
2025-06-26 21:36:39 +0200

    add `NameForm`

---
+++ src/components/assistant/name/form.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+
+import {
+  type NameFormSchema,
+  nameFormSchema,
+} from "@/components/assistant/name/schema"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+export function NameForm() {
+  const form = useForm<NameFormSchema>({
+    resolver: zodResolver(nameFormSchema),
+    defaultValues: {
+      name: "",
+    },
+    mode: "onSubmit",
+    reValidateMode: "onSubmit",
+  })
+
+  function onSubmit(values: NameFormSchema) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        noValidate
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="grid gap-4"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type="text" {...field} />
+              </FormControl>
+              <div className="min-h-[1.25rem]">
+                <FormMessage />
+              </div>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Anzeigen</Button>
+      </form>
+    </Form>
+  )
+}
