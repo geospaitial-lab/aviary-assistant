69ac6237a7b303bd9ed2efd0120e123c87a4ee3d
mrsmrynk <marius.maryniak@w-hs.de>
2025-07-08 00:04:29 +0200

    add `Area`

---
+++ src/components/assistant/area/area.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import * as React from "react"
+
+import { BoundingBoxForm } from "@/components/assistant/area/bounding-box/form"
+import { FileForm } from "@/components/assistant/area/file/form"
+import { Map } from "@/components/assistant/area/map/map"
+import { NameForm } from "@/components/assistant/area/name/form"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
+export function Area() {
+  const [activeTab, setActiveTab] = React.useState("name")
+
+  return (
+    <div className="@container">
+      <div className="flex flex-col @2xl:flex-row gap-4">
+        <div className="order-2 @2xl:order-1 @2xl:w-1/2 aspect-square">
+          <Map activeTab={activeTab} />
+        </div>
+
+        <div className="flex-1 order-1 @2xl:order-2">
+          <Tabs defaultValue="name" onValueChange={setActiveTab}>
+            <TabsList className="mb-2">
+              <TabsTrigger value="name">Name</TabsTrigger>
+              <TabsTrigger value="file">Datei</TabsTrigger>
+              <TabsTrigger value="bounding-box">Bounding Box</TabsTrigger>
+              <TabsTrigger value="draw">Zeichnen</TabsTrigger>
+            </TabsList>
+
+            <TabsContent value="name">
+              <div className="p-4 border rounded-lg">
+                <NameForm />
+              </div>
+            </TabsContent>
+
+            <TabsContent value="file">
+              <div className="p-4 border rounded-lg">
+                <FileForm />
+              </div>
+            </TabsContent>
+
+            <TabsContent value="bounding-box">
+              <div className="p-4 border rounded-lg">
+                <BoundingBoxForm />
+              </div>
+            </TabsContent>
+
+            <TabsContent value="draw">
+              <div className="p-4 border rounded-lg">
+                <p>TODO</p>
+              </div>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </div>
+  )
+}
