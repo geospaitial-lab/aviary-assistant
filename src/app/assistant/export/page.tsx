fde3b7457816a158a7aa82ce7743c33c30be7ee1
mrsmrynk <marius.maryniak@w-hs.de>
2025-07-09 16:19:42 +0200

    add export page

---
+++ src/app/assistant/export/page.tsx
@@ -0,0 +1,9 @@
+import { Export } from "@/components/assistant/export/export"
+
+export default function Page() {
+  return (
+    <div className="max-w-6xl mt-8 mx-auto px-4">
+      <Export />
+    </div>
+  )
+}
