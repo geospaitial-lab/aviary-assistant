9646de3684bf17274d4fa1cbd4eca4c61fb6f99a
mrsmrynk <marius.maryniak@w-hs.de>
2025-07-09 16:20:21 +0200

    refactor

--- src/app/assistant/area/page.tsx
+++ src/app/assistant/area/page.tsx
@@ -2,7 +2,7 @@ import { Area } from "@/components/assistant/area/area"
 
 export default function Page() {
   return (
-    <div className="max-w-6xl mt-8 mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="max-w-6xl mt-8 mx-auto px-4">
       <Area />
     </div>
   )
