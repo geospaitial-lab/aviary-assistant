407541d8a3fde407a2df12fc28dcb11a8aea7562
mrsmrynk <marius.maryniak@w-hs.de>
2025-07-25 15:38:44 +0200

    add schema

---
+++ src/components/assistant/model/schema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod"
+
+const ERROR_MODEL = "Mindestens ein Modell muss ausgewählt sein"
+
+export const modelFormSchema = z
+  .object({
+    model1: z.boolean(),
+    model2: z.boolean(),
+  })
+  .refine((data) => data.model1 || data.model2, {
+    message: ERROR_MODEL,
+    path: ["model1"],
+  })
+
+export type ModelFormSchema = z.infer<typeof modelFormSchema>
