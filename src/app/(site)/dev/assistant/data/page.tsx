0534cd97a3e61b5f5400554ecf5a06a4d455ae4a
mrsmrynk <marius.maryniak@w-hs.de>
2025-08-04 13:13:40 +0200

    add data page

---
+++ src/app/(site)/dev/assistant/data/page.tsx
@@ -0,0 +1,9 @@
+import { Data } from "@/components/assistant/data/data"
+
+export default function Page() {
+  return (
+    <div className="max-w-6xl mt-8 mx-auto px-4">
+      <Data />
+    </div>
+  )
+}
