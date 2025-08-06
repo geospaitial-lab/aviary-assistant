fe33d2fe80777884c31d63444073db00e7a026fc
mrsmrynk <marius.maryniak@w-hs.de>
2025-08-06 15:17:31 +0200

    edit schema

--- src/components/assistant/data/vrt/schema.ts
+++ src/components/assistant/data/vrt/schema.ts
@@ -1,8 +1,21 @@
 import { z } from "zod"
 
+const ERROR_NUMBER = "Muss eine Zahl zwischen 0 und 1 sein"
+
 export const vrtFormSchema = z.object({
   path: z.string(),
-  groundSamplingDistance: z.number(),
+  groundSamplingDistance: z.preprocess(
+    (value) => {
+      if (value === "") return undefined
+      return typeof value === "string" ? value.replace(/,/g, ".") : value
+    },
+    z.coerce
+      .number({
+        invalid_type_error: ERROR_NUMBER,
+      })
+      .min(0, ERROR_NUMBER)
+      .max(1, ERROR_NUMBER),
+  ),
   channels: z.enum(["rgb", "cir", "nir", "rgbi", "dom"]),
 })
 
