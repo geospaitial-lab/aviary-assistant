7b66ee701ee28acec0414f88c89a80922d4a6703
mrsmrynk <marius.maryniak@w-hs.de>
2025-08-14 15:31:47 +0200

    add fileName

--- src/components/assistant/area/file/store.ts
+++ src/components/assistant/area/file/store.ts
@@ -7,9 +7,11 @@ import { AllGeoJSON } from "@turf/helpers"
 
 interface FileState {
   formValues: FileFormSchema | null
+  fileName: string | null
   geoJson: AllGeoJSON | null
   isLoading: boolean
   setFormValues: (values: FileFormSchema) => void
+  setFileName: (fileName: string) => void
   setGeoJson: (geoJson: AllGeoJSON) => void
   setIsLoading: (isLoading: boolean) => void
   reset: () => void
@@ -21,9 +23,11 @@ export const useFileStore = create<FileState>()(
   persist(
     (set) => ({
       formValues: null,
+      fileName: null,
       geoJson: null,
       isLoading: false,
       setFormValues: (values) => set({ formValues: values }),
+      setFileName: (fileName) => set({ fileName }),
       setGeoJson: (geoJson) => {
         set({ geoJson })
         if (geoJson) {
@@ -38,7 +42,12 @@ export const useFileStore = create<FileState>()(
       },
       setIsLoading: (isLoading) => set({ isLoading: isLoading }),
       reset: () => {
-        set({ formValues: null, geoJson: null, isLoading: false })
+        set({
+          formValues: null,
+          fileName: null,
+          geoJson: null,
+          isLoading: false,
+        })
         idbDel(GEOJSON_KEY).catch((error) => {
           console.error("Error deleting file GeoJSON during reset:", error)
         })
@@ -49,6 +58,7 @@ export const useFileStore = create<FileState>()(
       storage: createJSONStorage(() => localStorage),
       partialize: (state) => ({
         formValues: state.formValues,
+        fileName: state.fileName,
       }),
       onRehydrateStorage: () => async (state) => {
         const savedGeoJson = await idbGet<AllGeoJSON>(GEOJSON_KEY)
