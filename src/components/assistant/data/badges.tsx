3ace7a9c4724101fd5a07056002a577c18c3e162
mrsmrynk <marius.maryniak@w-hs.de>
2025-08-29 09:27:52 +0200

    refactor

--- src/components/assistant/data/badges.tsx
+++ src/components/assistant/data/badges.tsx
@@ -1,31 +1,31 @@
 import { Badge } from "@/components/ui/badge"
 
 export function RgbBadge() {
-  return <Badge className="font-mono">RGB</Badge>
+  return <Badge className="font-mono select-none">RGB</Badge>
 }
 
 export function CirBadge() {
-  return <Badge className="font-mono">CIR</Badge>
+  return <Badge className="font-mono select-none">CIR</Badge>
 }
 
 export function NirBadge() {
-  return <Badge className="font-mono">NIR</Badge>
+  return <Badge className="font-mono select-none">NIR</Badge>
 }
 
 export function RgbiBadge() {
-  return <Badge className="font-mono">RGBI</Badge>
+  return <Badge className="font-mono select-none">RGBI</Badge>
 }
 
 export function DomBadge() {
-  return <Badge className="font-mono">DOM</Badge>
-}
-
-export function VrtBadge() {
-  return <Badge className="font-mono">VRT</Badge>
+  return <Badge className="font-mono select-none">DOM</Badge>
 }
 
 export function WmsBadge() {
-  return <Badge className="font-mono">WMS</Badge>
+  return <Badge className="font-mono select-none">WMS</Badge>
+}
+
+export function VrtBadge() {
+  return <Badge className="font-mono select-none">VRT</Badge>
 }
 
 export interface ChannelBadgeProps {
@@ -45,13 +45,13 @@ export function ChannelBadge({ channel }: ChannelBadgeProps) {
 }
 
 export interface DataBadgeProps {
-  data: "vrt" | "wms"
+  data: "wms" | "vrt"
 }
 
 export function DataBadge({ data }: DataBadgeProps) {
   const dataMap = {
-    vrt: <VrtBadge />,
     wms: <WmsBadge />,
+    vrt: <VrtBadge />,
   }
 
   return dataMap[data]
