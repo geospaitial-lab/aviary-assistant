5ec63c632e71aa5aec026b8b6de73f17e74b53df
mrsmrynk <marius.maryniak@w-hs.de>
2025-08-30 20:56:18 +0200

    add getStore

--- src/components/assistant/summary/config-parser.ts
+++ src/components/assistant/summary/config-parser.ts
@@ -1,5 +1,89 @@
+import { type BoundingBoxFormSchema } from "@/components/assistant/area/bounding-box/schema"
 import { useBoundingBoxStore } from "@/components/assistant/area/bounding-box/store"
+import { type FileFormSchema } from "@/components/assistant/area/file/schema"
+import { useFileStore } from "@/components/assistant/area/file/store"
+import { type NameFormSchema } from "@/components/assistant/area/name/schema"
+import { useNameStore } from "@/components/assistant/area/name/store"
 import { useAreaStore } from "@/components/assistant/area/store"
+import { useDataStore } from "@/components/assistant/data/store"
+import { type DataSource } from "@/components/assistant/data/store"
+import { type ExportFormSchema } from "@/components/assistant/export/schema"
+import { useExportStore } from "@/components/assistant/export/store"
+import { type ModelFormSchema } from "@/components/assistant/model/schema"
+import { useModelStore } from "@/components/assistant/model/store"
+import { type CpuFormSchema } from "@/components/assistant/resources/cpu/schema"
+import { useCpuStore } from "@/components/assistant/resources/cpu/store"
+import { type GpuFormSchema } from "@/components/assistant/resources/gpu/schema"
+import { useGpuStore } from "@/components/assistant/resources/gpu/store"
+import { useResourcesStore } from "@/components/assistant/resources/store"
+
+interface Store {
+  model: {
+    formValues: ModelFormSchema | null
+  }
+  area: {
+    activeTab: string
+    boundingBox: {
+      formValues: BoundingBoxFormSchema | null
+    }
+    file: {
+      formValues: FileFormSchema | null
+    }
+    name: {
+      formValues: NameFormSchema | null
+    }
+  }
+  data: {
+    dataSources: DataSource[]
+  }
+  resources: {
+    activeTab: string
+    cpu: {
+      formValues: CpuFormSchema | null
+    }
+    gpu: {
+      formValues: GpuFormSchema | null
+    }
+  }
+  export: {
+    formValues: ExportFormSchema | null
+  }
+}
+
+export function getStore(): Store {
+  return {
+    model: {
+      formValues: useModelStore.getState().formValues,
+    },
+    area: {
+      activeTab: useAreaStore.getState().activeTab,
+      boundingBox: {
+        formValues: useBoundingBoxStore.getState().formValues,
+      },
+      file: {
+        formValues: useFileStore.getState().formValues,
+      },
+      name: {
+        formValues: useNameStore.getState().formValues,
+      },
+    },
+    data: {
+      dataSources: useDataStore.getState().dataSources,
+    },
+    resources: {
+      activeTab: useResourcesStore.getState().activeTab,
+      cpu: {
+        formValues: useCpuStore.getState().formValues,
+      },
+      gpu: {
+        formValues: useGpuStore.getState().formValues,
+      },
+    },
+    export: {
+      formValues: useExportStore.getState().formValues,
+    },
+  }
+}
 
 function parseAreaConfig(): string[] {
   const TILE_SIZE = 128
