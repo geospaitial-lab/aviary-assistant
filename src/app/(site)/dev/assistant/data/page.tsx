ef658624719f6d8131195aaa1815d5862aee5d8c
mrsmrynk <marius.maryniak@w-hs.de>
2025-11-09 18:57:50 +0100

    remove dev sites

--- src/app/(site)/dev/assistant/data/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Data } from "@/components/assistant/data/data"
-
-export default function Page() {
-  return (
-    <div className="max-w-6xl mt-8 mx-auto px-4">
-      <Data />
-    </div>
-  )
-}
