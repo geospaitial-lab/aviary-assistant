4de9f3c702128e8fb8449ba0783e6cea21b6319e
mrsmrynk <marius.maryniak@w-hs.de>
2025-11-11 14:35:20 +0100

    add copyright / license notice

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2025 Marius Maryniak
+ *
+ * This file is part of aviary-assistant.
+ *
+ * aviary-assistant is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * aviary-assistant is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with aviary-assistant.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
 import { Link } from "@/components/link"
 
 export function Footer() {
@@ -38,7 +54,9 @@ export function Footer() {
                     <Link className="text-sm">Anleitungen</Link>
                   </li>
                   <li>
-                    <Link href="/faq" className="text-sm">FAQ</Link>
+                    <Link href="/faq" className="text-sm">
+                      FAQ
+                    </Link>
                   </li>
                   <li>
                     <Link className="text-sm">Community</Link>
