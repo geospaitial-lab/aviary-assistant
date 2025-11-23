e59ae3430a2e88d0695f42336ddbf15fb6c61d05
mrsmrynk <marius.maryniak@w-hs.de>
2025-11-23 20:23:15 +0100

    edit area faq

--- src/components/faq/area-faq.tsx
+++ src/components/faq/area-faq.tsx
@@ -40,20 +40,6 @@ export function AreaFaq() {
             Bitte konvertiere deine .geojson-Datei gegebenenfalls entsprechend.
           </AccordionContent>
         </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>Gebiet 2</AccordionTrigger>
-          <AccordionContent>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
-            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-            no sea takimata sanctus est Lorem ipsum dolor sit amet.
-          </AccordionContent>
-        </AccordionItem>
       </Accordion>
     </div>
   )
